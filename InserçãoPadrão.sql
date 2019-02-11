83bea76f101eeab22886a80be87ac09a9dce82bd
Franklin Bitencourt Dantas <47326175810@info.local>
2019-02-11 11:30:03 -0200

    Importaçao de dados iniciada

--- InserçãoPadrão.sql
+++ InserçãoPadrão.sql
@@ -1,11 +1,9 @@
 /*******************
-***INSERT padrões **
+***INSERT Iniciais **
 ********************/
 
--- Va para Inserção.sql , lá tem a maneira correta de adicionar dados
-
 -- Especialidade padrão
-INSERT INTO Especialidade(NOME) VALUES ('Cardiologia'),('Oftamologia'),('Radiologia');
+INSERT INTO Especialidade(NOME) VALUES ('Acupuntura'),('Anestesiologia'),('Angiologia'),('Cardiologia'),('Cirurgia Cardiovascular'),('Cirurgia de mão'),('Cirurgia do aparelho digestivo'),('Cirurgia geral'),('Cirurgia pediátrica'),('Cirurgia plástica'),('Cirurgia torácica'),('Cirurgia vascular'),('Dermatologia'),('Radioterapia'),('Urologia'),('Pediatria'),('Psiquiatria');
 
 -- Tipos de usuario padrão
 INSERT INTO Tipo_Usuario VALUES('Administrador'),('Medico'),('Paciente');
@@ -15,4 +13,4 @@ INSERT INTO StatusConsulta VALUES('Em espera'),('Confirmado'),('Feita');
 
 -- Administrador padrão
 INSERT INTO Usuario(EMAIL,SENHA,ID_TIPO_USUARIO) VALUES('Administrador','123456778',1);
-INSERT INTO Administrador(ID_USUARIO,NOME)VALUES (1,'Administrador');
+
--- Procedures.sql
+++ Procedures.sql
@@ -1,42 +1,53 @@
 --- PROCEDURES ---
 
---Procura medico por ID e retorna o nome e o CRM
+--Procurar medico por ID e retorna o nome e o CRM
 CREATE PROCEDURE ProcurarMedicoPorID @ID INT
 AS
 BEGIN
-SELECT MED.NOME,MED.CRM FROM VerMedicos AS MED WHERE ID = @ID;
+SELECT MED.NOME,MED.CRM FROM VerMedicos AS MED WHERE MED.ID = @ID;
 END
 
 -- Retorna todas as informações do Medico
 CREATE PROCEDURE VerTudoMedico @ID INT
 AS
 BEGIN
-SELECT * FROM VerMedicos WHERE ID = @ID;
+SELECT * FROM VerMedicos AS MED WHERE MED.ID = @ID;
 END
 
 -- Retorna o nome e o email do medico
 CREATE PROCEDURE ProcurarUsuarioMedico @ID INT
 AS
 BEGIN
-SELECT MED.NOME,MED.EMAIL VerMedicos FROM Medico AS MED;
+SELECT MED.NOME,MED.EMAIL VerMedicos FROM Medico AS MED WHERE MED.ID = @ID;
 END
 
-/*Procura paciente*/
+/*Procurar paciente*/
 
+-- Procurar todas as informações do paciente no ID selecionado
 CREATE PROCEDURE ProcurarPacientePorID @ID INT
 AS
 BEGIN
-SELECT * FROM VerPacientes WHERE ID = @ID;
+SELECT * FROM VerPacientes AS PAC WHERE PAC.ID = @ID;
 END
 
+-- Procurar todas as informações do paciente com o ID selecionado
 CREATE PROCEDURE ProcurarPacientePorCPF @CPF CHAR(10) 
 AS
 BEGIN
-SELECT * FROM VerPacientes WHERE CPF = @CPF;
+SELECT * FROM VerPacientes AS PAC WHERE PAC.CPF = @CPF;
 END
 
+-- Procurar nome e email do usuario com o ID selecionado
 CREATE PROCEDURE ProcuraUsuarioPaciente @ID INT
+AS
 BEGIN
-SELECT PAC.NOME,PAC.EMAIL FROM VerPacientes AS PAC;
+SELECT PAC.NOME,PAC.EMAIL FROM VerPacientes AS PAC WHERE PAC.ID = @ID;
 END
 
+/*Procurar clinica*/
+-- Retorna todas as informações da clinica
+CREATE PROCEDURE ProcurarClinicaPorID @ID INT
+AS 
+BEGIN
+SELECT * FROM ProcurarClinicas WHERE ID = @ID;
+END
